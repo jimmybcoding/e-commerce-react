bc25c04166ded31ad286c56d03e8ec6f6dba84cf
James Barbour <jameskbarbour1@gmail.com>
2024-03-04 21:42:32 -0500

    Change to react hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     return (
     <>
         <Nav />
-        <Products />
         <Routes >
             <Route path= '/' element={<Products />}/>
             <Route path='cart' element={<Cart />} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
 )
