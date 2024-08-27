34069a4673eab2f879a5b7b6632c14c91a7900fc
James Barbour <jameskbarbour1@gmail.com>
2024-08-27 09:59:20 -0400

    refactor component

--- src/utilities/ProductModal.jsx
+++ src/utilities/ProductModal.jsx
@@ -1,88 +1,125 @@
 import { useContext, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight, faStar, faStarHalfStroke, faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { faStar as farStar } from  "@fortawesome/free-regular-svg-icons" 
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
 import { CartContext } from "../context/CartContext";
 
 function ProductModal({ selectedProduct, isOpen, toggleOpen }) {
+  const [imageIndex, setImageIndex] = useState(0);
+  const { cartItems, addToCart, removeFromCart } = useContext(CartContext);
 
-const [imageIndex, setImageIndex] = useState(0);
-const { cartItems, addToCart, removeFromCart} = useContext(CartContext);
+  const {
+    title,
+    images,
+    price,
+    stock,
+    rating,
+  } = selectedProduct;
 
-const checkStarRating = () => {
-   if (selectedProduct.rating >= 4.25){   
-    return ( 
-    <>
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStarHalfStroke} />
-    </>
-    )} else if (selectedProduct.rating < 4.25 && selectedProduct.rating >= 3.75) {
-      return (
-    <>
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={farStar} />
-    </>        
-      )
-    } else {
-      return (
-           <>
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStar} />
-      <FontAwesomeIcon icon={faStarHalfStroke} />
-      <FontAwesomeIcon icon={farStar} />
-    </> 
-      )
-    } 
-}
+  const renderStarRating = () => {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 >= 0.75;
+    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
 
-const checkIndex = () => {
-  imageIndex === selectedProduct.images.length-1 ? setImageIndex(0) : setImageIndex(imageIndex + 1)
-}
+    return (
+      <>
+        {Array(fullStars)
+          .fill()
+          .map((_, i) => (
+            <FontAwesomeIcon key={`full-star-${i}`} icon={faStar} />
+          ))}
+        {halfStar && <FontAwesomeIcon icon={faStarHalfStroke} />}
+        {Array(emptyStars)
+          .fill()
+          .map((_, i) => (
+            <FontAwesomeIcon key={`empty-star-${i}`} icon={farStar} />
+          ))}
+      </>
+    );
+  };
+
+  const handleImageChange = (direction) => {
+    setImageIndex((prevIndex) =>
+      direction === "left"
+        ? prevIndex === 0
+          ? images.length - 1
+          : prevIndex - 1
+        : prevIndex === images.length - 1
+        ? 0
+        : prevIndex + 1
+    );
+  };
+
+  const isProductInCart = cartItems.find(
+    (cartItem) => cartItem.id === selectedProduct.id
+  );
 
   return (
     <>
-      {isOpen && (  
-        <>
-          <div className="products-modal-overlay">
-            <h1>{selectedProduct.title}</h1>
-            <button className="red" onClick={toggleOpen}>X</button>
-            <div className="container">
-              <div className="products-modal-container">
-                <button className="products-modal-btn" onClick={() => checkIndex()}>{<FontAwesomeIcon icon={faArrowLeft} />}</button>             
-                <img src={selectedProduct.images[imageIndex]} className="modal-thumbnail" />
-                <button className="products-modal-btn" onClick={() => checkIndex()}>{<FontAwesomeIcon icon={faArrowRight} />}</button>
-              </div>
-              <div className="products-modal-details">
-                <p>{`Image ` + parseInt(imageIndex + 1)}{` of `+ parseInt(selectedProduct.images.length)}</p>
-                <p>{`$` + selectedProduct.price}</p>
-                <p>{`In Stock: ` + selectedProduct.stock}</p>
-                <p>{`Customer Rating: ` + selectedProduct.rating}</p>
-                <span className="stars">{checkStarRating()}</span>
-              </div>
-              {cartItems.find((cartItem) => cartItem.id === selectedProduct.id) ? 
-                <div className="products_btn_container">
-                  <button className="quantity-adjust-btn" onClick={() => addToCart(selectedProduct)}>+</button>
-                  <div>{cartItems.find(cartItem => cartItem.id === selectedProduct.id).quantity +'\n In Cart'}</div>
-                  <button className="quantity-adjust-btn" onClick={() => removeFromCart(selectedProduct)}>-</button>
-                </div> :
-                <div className="primary-btn-container"> 
-                  <button className="add-btn" onClick={() => addToCart(selectedProduct)}>
-                    Add
-                    <FontAwesomeIcon icon={faCartShopping} />  
-                  </button>
-                  </div>}
+      {isOpen && (
+        <div className="products-modal-overlay">
+          <h1>{title}</h1>
+          <button className="red" onClick={toggleOpen}>
+            X
+          </button>
+          <div className="container">
+            <div className="products-modal-container">
+              <button
+                className="products-modal-btn"
+                onClick={() => handleImageChange("left")}
+              >
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </button>
+              <img src={images[imageIndex]} className="modal-thumbnail" />
+              <button
+                className="products-modal-btn"
+                onClick={() => handleImageChange("right")}
+              >
+                <FontAwesomeIcon icon={faArrowRight} />
+              </button>
             </div>
+            <div className="products-modal-details">
+              <p>{`Image ${imageIndex + 1} of ${images.length}`}</p>
+              <p>{`$${price}`}</p>
+              <p>{`In Stock: ${stock}`}</p>
+              <p>{`Customer Rating: ${rating}`}</p>
+              <span className="stars">{renderStarRating()}</span>
+            </div>
+            {isProductInCart ? (
+              <div className="products_btn_container">
+                <button
+                  className="quantity-adjust-btn"
+                  onClick={() => addToCart(selectedProduct)}
+                >
+                  +
+                </button>
+                <div>
+                  {isProductInCart.quantity + "\n In Cart"}
+                </div>
+                <button
+                  className="quantity-adjust-btn"
+                  onClick={() => removeFromCart(selectedProduct)}
+                >
+                  -
+                </button>
+              </div>
+            ) : (
+              <div className="primary-btn-container">
+                <button
+                  className="add-btn"
+                  onClick={() => addToCart(selectedProduct)}
+                >
+                  Add
+                  <FontAwesomeIcon icon={faCartShopping} />
+                </button>
+              </div>
+            )}
           </div>
-        </>)}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default ProductModal
+export default ProductModal;
+
